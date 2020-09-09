b6cfe87844bdef1914d8ef65c02e42ac97356928
dasjideepak <dasjideepak@gmail.com>
2020-09-09 19:36:30 +0530

    header complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Header from "../src/components/Header";
-import Footer from "../src/components/Footer";
 
 function App() {
   return (
     <div>
       <Header />
-      <Footer />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { IoIosSearch } from "react-icons/io";
+import { VscEllipsis } from "react-icons/vsc";
+import { AiFillBell } from "react-icons/ai";
+
+const NavLinks = (props) => {
+  return <a href={props.url}>{props.name}</a>;
+};
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="container flex">
+        <a href="##">
+          <img className="logo" src="/images/logo.png" alt="logo" />
+        </a>
+        <div className="search-bar flex">
+          <IoIosSearch className="icon" />
+          <input
+            type="text"
+            placeholder="Discover your next favorite thing..."
+          />
+        </div>
+        <div className="nav-user-flex">
+          <div className="nav-links-sec flex">
+            <NavLinks url="##" name="Deals" />
+            <NavLinks url="##" name="Jobs" />
+            <NavLinks url="##" name="Discussions" />
+            <NavLinks url="##" name="Ship" />
+            <NavLinks url="##" name="Mentors" />
+            <button
+              onClick={() => console.log("Button Clicked")}
+              className="flex"
+            >
+              <VscEllipsis className="icon" />
+            </button>
+          </div>
+          <div className="user-login-display">
+            <NavLinks url="##" name="Post" />
+            <AiFillBell className="icon" />
+            <img
+              src="https://ph-avatars.imgix.net/2390606/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=40&h=40&fit=crop&dpr=2"
+              alt="user-avatar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
