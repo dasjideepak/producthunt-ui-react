af96949c8493c95953834c80ef042bec9b59f37b
dasjideepak <dasjideepak@gmail.com>
2020-09-09 16:42:54 +0530

    netlify config

---
+++ build/precache-manifest.f2624c17bc1043e0b2b784a93b5ec2bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d45e836c191148ad043c0f8df30d2f12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c079597b16e71645a099",
+    "url": "/static/css/main.40e8d1ba.chunk.css"
+  },
+  {
+    "revision": "bd0cc8af3552cdfbb8f6",
+    "url": "/static/js/2.219a14a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.219a14a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c079597b16e71645a099",
+    "url": "/static/js/main.5c545cd3.chunk.js"
+  },
+  {
+    "revision": "202fce87607a16d29b82",
+    "url": "/static/js/runtime-main.57e54692.js"
+  }
+]);
